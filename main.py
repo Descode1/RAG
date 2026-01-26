c03a0a1d0b5a4e20c63ce26deb8ac658dd076cf8
Davy <103334758+Descode1@users.noreply.github.com>
2026-01-26 17:49:40 +0200

    chunk embedding

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import ollama
+EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+
+VECTOR_DB = []
 
 # Loading dataset into memory
 dataset = []
 with open("cat-facts.txt", "r", encoding="utf-8") as file:
     dataset = file.readlines()
     print(f"Loaded {len(dataset)} entries")
+
+def add_chunk_to_database(chunk):
+    embeddings = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
+    VECTOR_DB.append((chunk,embeddings))
+
+for i, chunk in enumerate(dataset):
+    add_chunk_to_database(chunk)
+    print(f"Added chunk {i+1}/{len(dataset)} to the database")
