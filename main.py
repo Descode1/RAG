db104269b435e225afcf3af42a7c3d2feb73d662
Davy <103334758+Descode1@users.noreply.github.com>
2026-01-26 17:36:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import ollama
+
+# Loading dataset into memory
+dataset = []
+with open("cat-facts.txt", "r", encoding="utf-8") as file:
+    dataset = file.readlines()
+    print(f"Loaded {len(dataset)} entries")
